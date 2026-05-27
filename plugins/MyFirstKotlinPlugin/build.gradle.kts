8273e1c095135756dfbd905989c653e0679b02b8
xanderlol165-creator <xanderlol165@gmail.com>
2026-05-27 06:50:32 +0530

    Update build.gradle.kts

--- plugins/MyFirstKotlinPlugin/build.gradle.kts
+++ plugins/MyFirstKotlinPlugin/build.gradle.kts
@@ -1,16 +1,7 @@
-version = "1.0.0" 
-description = "Instantly toggle all avatars on and off." 
+version = "1.0.0"
+description = "Instantly toggle all avatars on and off."
 
 aliucord {
-    // This line forces the app to show your name instead of 'yournamehere'
-    author("Xander") 
-
-    changelog.set(
-        """
-        # 1.0.0
-        * Added a toggle switch to hide all avatars!
-        """.trimIndent(),
-    )
-    
+    author("Xander")
     deploy.set(true)
 }

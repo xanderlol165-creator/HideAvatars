6794d67e46143e2471b85cedc73fab39e22c3b0a
xanderlol165-creator <xanderlol165@gmail.com>
2026-05-27 05:55:28 +0530

    Update MyFirstKotlinPlugin.kt

--- plugins/MyFirstKotlinPlugin/src/main/kotlin/com/github/yournamehere/MyFirstKotlinPlugin.kt
+++ plugins/MyFirstKotlinPlugin/src/main/kotlin/com/github/yournamehere/MyFirstKotlinPlugin.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.api.SettingsAPI
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.after
+import com.aliucord.widgets.BottomSheet
+import com.discord.views.CheckedSetting
 import com.facebook.drawee.view.SimpleDraweeView
 
 import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
@@ -20,12 +24,17 @@ import com.discord.widgets.friends.WidgetFriendsListAdapter
 @AliucordPlugin
 class HideAvatars : Plugin() {
 
+    init {
+        settingsTab = SettingsTab(HideAvatarsSettings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
+    }
+
     override fun start(context: Context) {
         
         tryPatch("WidgetChatListAdapterItemMessage") {
             patcher.after<WidgetChatListAdapterItemMessage>(
                 "onConfigure", Int::class.java, Any::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 val root = itemView as? ViewGroup ?: return@after
                 hideAllDraweeViews(root)
             }
@@ -35,6 +44,7 @@ class HideAvatars : Plugin() {
             patcher.after<WidgetChatListAdapterItemEphemeralMessage>(
                 "onConfigure", Int::class.java, Any::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 val root = itemView as? ViewGroup ?: return@after
                 hideAllDraweeViews(root)
             }
@@ -44,6 +54,7 @@ class HideAvatars : Plugin() {
             patcher.after<WidgetHomeHeaderManager>(
                 "onViewBound", View::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 val view = it.args[0] as? ViewGroup ?: return@after
                 hideAllDraweeViews(view)
             }
@@ -53,6 +64,7 @@ class HideAvatars : Plugin() {
             patcher.after<UserProfileHeaderView>(
                 "updateAvatar", String::class.java, Boolean::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 (this as? ViewGroup)?.let { vg -> hideAllDraweeViews(vg) }
             }
         }
@@ -61,6 +73,7 @@ class HideAvatars : Plugin() {
             patcher.after<WidgetUserSheet>(
                 "onViewCreated", View::class.java, Bundle::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 val view = it.args[0] as? ViewGroup ?: return@after
                 hideAllDraweeViews(view)
             }
@@ -72,6 +85,7 @@ class HideAvatars : Plugin() {
                 RecyclerView.ViewHolder::class.java,
                 Int::class.java
             ) {
+                if (!settings.getBool("hide_avatars_toggle", true)) return@after
                 val holder = it.args[0] as? RecyclerView.ViewHolder ?: return@after
                 val root = holder.itemView as? ViewGroup ?: return@after
                 hideAllDraweeViews(root)
@@ -96,5 +110,20 @@ class HideAvatars : Plugin() {
                 is ViewGroup        -> hideAllDraweeViews(child)
             }
         }
+    }
+}
+
+class HideAvatarsSettings(private val settings: SettingsAPI) : BottomSheet() {
+    override fun onViewCreated(view: View, bundle: Bundle?) {
+        super.onViewCreated(view, bundle)
+        val context = view.context
+        
+        val switch = Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Hide Avatars", "Instantly toggle all avatars on and off.")
+        switch.isChecked = settings.getBool("hide_avatars_toggle", true)
+        switch.setOnCheckedListener {
+            settings.setBool("hide_avatars_toggle", it)
+        }
+        
+        addView(switch)
     }
 }

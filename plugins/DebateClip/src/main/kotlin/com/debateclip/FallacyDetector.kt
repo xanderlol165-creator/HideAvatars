91637a89e4e1d0d13e3969842ea780d263ba9e3f
xanderlol165-creator <xanderlol165@gmail.com>
2026-07-05 01:52:11 +0530

    Update FallacyDetector.kt

--- plugins/DebateClip/src/main/kotlin/com/debateclip/FallacyDetector.kt
+++ plugins/DebateClip/src/main/kotlin/com/debateclip/FallacyDetector.kt
@@ -1,21 +1,20 @@
-
 package com.debateclip
 
 import java.util.regex.Pattern
 
 object FallacyDetector {
 
-    private val patterns: List<Pair<String, Regex>> = listOf(
-        "Ad hominem" to Pattern.compile("\\b(idiot|stupid|moron|dumbass|dumb|clown|braindead|pathetic|you people|typical of (you|them))\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Strawman" to Pattern.compile("\\bso (you're|you are|what you're) (saying|basically saying)|\\byou basically (want|think)\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Whataboutism (tu quoque)" to Pattern.compile("\\b(but )?what about\\b|\\byou (do|did) it too\\b|\\byou're one to talk\\b|\\bhypocrite\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Appeal to authority" to Pattern.compile("\\b(experts|scientists|doctors|studies) (say|show|agree|prove)\\b|\\bit'?s (a )?(well[- ]known|proven) fact\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Appeal to popularity" to Pattern.compile("\\beveryone (knows|agrees|thinks)\\b|\\bmost people (agree|think|know)\\b|\\bnobody (believes|thinks) that\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Slippery slope" to Pattern.compile("\\bnext thing (you know)?\\b|\\bbefore you know it\\b|\\bwhere does it end\\b|\\bslippery slope\\b|\\bit will (inevitably )?lead to\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "False dilemma" to Pattern.compile("\\byou'?re either\\b|\\beither .{1,40} or (else)?\\b|\\bonly two (options|choices)\\b|\\bthere'?s no middle ground\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Hasty generalization" to Pattern.compile("\\ball (of (them|you)|men|women|people|\\w+s) (are|do|think)\\b|\\b(always|never) (does|do|works|happens)\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Appeal to emotion" to Pattern.compile("\\bthink of the children\\b|\\bhow dare you\\b|\\byou should be ashamed\\b", Pattern.CASE_INSENSITIVE).toRegex(),
-        "Appeal to ignorance" to Pattern.compile("\\bno one has (ever )?proven\\b|\\byou can'?t prove\\b|\\bthere'?s no evidence against\\b", Pattern.CASE_INSENSITIVE).toRegex()
+    private val patterns: List<Pair<String, Pattern>> = listOf(
+        "Ad hominem" to Pattern.compile("\\b(idiot|stupid|moron|dumbass|dumb|clown|braindead|pathetic|you people|typical of (you|them))\\b", Pattern.CASE_INSENSITIVE),
+        "Strawman" to Pattern.compile("\\bso (you're|you are|what you're) (saying|basically saying)|\\byou basically (want|think)\\b", Pattern.CASE_INSENSITIVE),
+        "Whataboutism (tu quoque)" to Pattern.compile("\\b(but )?what about\\b|\\byou (do|did) it too\\b|\\byou're one to talk\\b|\\bhypocrite\\b", Pattern.CASE_INSENSITIVE),
+        "Appeal to authority" to Pattern.compile("\\b(experts|scientists|doctors|studies) (say|show|agree|prove)\\b|\\bit'?s (a )?(well[- ]known|proven) fact\\b", Pattern.CASE_INSENSITIVE),
+        "Appeal to popularity" to Pattern.compile("\\beveryone (knows|agrees|thinks)\\b|\\bmost people (agree|think|know)\\b|\\bnobody (believes|thinks) that\\b", Pattern.CASE_INSENSITIVE),
+        "Slippery slope" to Pattern.compile("\\bnext thing (you know)?\\b|\\bbefore you know it\\b|\\bwhere does it end\\b|\\bslippery slope\\b|\\bit will (inevitably )?lead to\\b", Pattern.CASE_INSENSITIVE),
+        "False dilemma" to Pattern.compile("\\byou'?re either\\b|\\beither .{1,40} or (else)?\\b|\\bonly two (options|choices)\\b|\\bthere'?s no middle ground\\b", Pattern.CASE_INSENSITIVE),
+        "Hasty generalization" to Pattern.compile("\\ball (of (them|you)|men|women|people|\\w+s) (are|do|think)\\b|\\b(always|never) (does|do|works|happens)\\b", Pattern.CASE_INSENSITIVE),
+        "Appeal to emotion" to Pattern.compile("\\bthink of the children\\b|\\bhow dare you\\b|\\byou should be ashamed\\b", Pattern.CASE_INSENSITIVE),
+        "Appeal to ignorance" to Pattern.compile("\\bno one has (ever )?proven\\b|\\byou can'?t prove\\b|\\bthere'?s no evidence against\\b", Pattern.CASE_INSENSITIVE)
     )
 
     private data class Flag(val fallacy: String, val example: String)
@@ -24,9 +23,11 @@ object FallacyDetector {
         val perAuthor = LinkedHashMap<String, MutableList<Flag>>()
         for (line in lines) {
             val flags = perAuthor.getOrPut(line.author) { mutableListOf() }
-            for ((name, rx) in patterns) {
-                val match = rx.find(line.content) ?: continue
-                flags.add(Flag(name, match.value.trim()))
+            for ((name, pattern) in patterns) {
+                val matcher = pattern.matcher(line.content)
+                if (matcher.find()) {
+                    flags.add(Flag(name, matcher.group().trim()))
+                }
             }
         }
 
